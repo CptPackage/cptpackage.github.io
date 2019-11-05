5983b65e8ee94ac8168b28ddb53ecb52865e932e
CptPackage <abdelrhmanabdalla15@gmail.com>
2019-11-05 15:51:11 +0100

    Updates

---
+++ precache-manifest.cdfa5f55226851a11253ff0ada55c2b1.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2228a3d88d61763ceabb2eec0d8c8a9c",
+    "url": "/bella/index.html"
+  },
+  {
+    "revision": "6a6ed0401169f1b08a8f",
+    "url": "/bella/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "fc7f4f38809a696516b6",
+    "url": "/bella/static/css/main.4b52222b.chunk.css"
+  },
+  {
+    "revision": "6a6ed0401169f1b08a8f",
+    "url": "/bella/static/js/2.a363a698.chunk.js"
+  },
+  {
+    "revision": "fc7f4f38809a696516b6",
+    "url": "/bella/static/js/main.f30b64d0.chunk.js"
+  },
+  {
+    "revision": "8fd6435261c88d0c4fbb",
+    "url": "/bella/static/js/runtime-main.71ffa13b.js"
+  },
+  {
+    "revision": "85a97377d900b65f917ec17959a53471",
+    "url": "/bella/static/media/Banner-Illustration.85a97377.svg"
+  },
+  {
+    "revision": "415d0a87997af3ab8e93ddad8bb86031",
+    "url": "/bella/static/media/BasilicaDiSanPietro-poster.415d0a87.jpg"
+  },
+  {
+    "revision": "95b2c9d3404f5171069fe735acd768bf",
+    "url": "/bella/static/media/Congratulations-Icon.95b2c9d3.svg"
+  },
+  {
+    "revision": "09c0503c0c8e82921e155fec680e392e",
+    "url": "/bella/static/media/Corrent-Icon.09c0503c.svg"
+  },
+  {
+    "revision": "25c3c9ed35eb1d5de1e4597b2a915a75",
+    "url": "/bella/static/media/Facebook-Hovered.25c3c9ed.svg"
+  },
+  {
+    "revision": "2abc5aeb1c3db4381dda65ea3fc54fb2",
+    "url": "/bella/static/media/Facebook-Normal.2abc5aeb.svg"
+  },
+  {
+    "revision": "eda6a4bf2e1eae974154d283f28f1c81",
+    "url": "/bella/static/media/FontanaDiTrevi-poster.eda6a4bf.jpg"
+  },
+  {
+    "revision": "622957f19e7635e8fc099aab9735be44",
+    "url": "/bella/static/media/GPS-Icon.622957f1.svg"
+  },
+  {
+    "revision": "c194d41cee52a44de6516afd0e66e007",
+    "url": "/bella/static/media/Incorrect-Icon.c194d41c.svg"
+  },
+  {
+    "revision": "ad16263ac1b6123783d026437113fab8",
+    "url": "/bella/static/media/Instagram-Hovered.ad16263a.svg"
+  },
+  {
+    "revision": "a4c23b7988fa96f0c960f17d3b083e96",
+    "url": "/bella/static/media/Instagram-Normal.a4c23b79.svg"
+  },
+  {
+    "revision": "243df3a767961ec31cdef44129f37f88",
+    "url": "/bella/static/media/Logo.243df3a7.png"
+  },
+  {
+    "revision": "e635172c7803b5900599a20b1d7daec3",
+    "url": "/bella/static/media/Section-1-icon.e635172c.svg"
+  },
+  {
+    "revision": "61b59119b66a0ee3d6c4accb52eafedb",
+    "url": "/bella/static/media/Section-2-icon.61b59119.svg"
+  },
+  {
+    "revision": "1a0132e12a4859035b4790d1fec4a504",
+    "url": "/bella/static/media/Section-3-icon.1a0132e1.svg"
+  },
+  {
+    "revision": "7db27e6b9a51dad47d30d91b62105927",
+    "url": "/bella/static/media/Twitter-Hovered.7db27e6b.svg"
+  },
+  {
+    "revision": "66c923f7c5f1650b86196cca1769d836",
+    "url": "/bella/static/media/Twitter-Normal.66c923f7.svg"
+  },
+  {
+    "revision": "7765fe07780af9d6ee78a8f0de749306",
+    "url": "/bella/static/media/Upload-Icon.7765fe07.svg"
+  }
+]);
